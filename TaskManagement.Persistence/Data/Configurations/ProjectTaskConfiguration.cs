99365d0d36d0f0313d78b6a48916b107d96e2183
Youssef Taha <yousif.t.abdulwahab@gmail.com>
2026-05-22 17:31:16 +0300

    feat: implement ApplicationDbContext and Fluent API entity configurations

---
+++ TaskManagement.Persistence/Data/Configurations/ProjectTaskConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domin.Models;
+
+namespace TaskManagement.Persistence.Data.Configurations
+{
+    public class ProjectTaskConfiguration : IEntityTypeConfiguration<ProjectTask>
+    {
+        public void Configure(EntityTypeBuilder<ProjectTask> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(t => t.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(t => t.Status)
+                .HasConversion<int>()
+                .IsRequired();
+
+            builder.Property(t => t.Priority)
+                .HasConversion<int>()
+                .IsRequired();
+
+            builder.HasIndex(t => t.ProjectId);
+        }
+    }
+}
