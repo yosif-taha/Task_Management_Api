c923be79ecf0287f7cf688e62545c03f6f33b965
Youssef Taha <yousif.t.abdulwahab@gmail.com>
2026-05-22 23:01:51 +0300

    feat: integrate FluentValidation with MediatR Pipeline Behavior

--- TaskManagement.Application/DependancyInjection.cs
+++ TaskManagement.Application/DependancyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using TaskManagement.Application.Common.Behaviors;
 
 namespace TaskManagement.Application
 {
@@ -7,9 +10,13 @@ namespace TaskManagement.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-        
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
 
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             return services;
         }
